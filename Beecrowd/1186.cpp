866e736102e08208fab861d6e0c0a490e322b58b
Maruf Rayhan <marufrayhan2002@gmail.com>
2024-12-07 21:30:13 +0600

    Added 1186.cpp

---
+++ Beecrowd/1186.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int M = 12;
+    double arr[M][M];
+    double sum = 0;
+
+    char operation;
+    cin >> operation;
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cin >> arr[i][j];
+        }    
+    }
+    
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = M - i; j < M; j++)
+        {
+            sum += arr[i][j];
+        }
+    }
+
+    if (operation == 'S')
+    {
+        cout << fixed << setprecision(1) << sum << endl;
+    }
+    else if (operation == 'M')
+    {
+        cout << fixed << setprecision(1) << sum / 66 << endl;
+    }
+    
+
+    return 0;
+}
